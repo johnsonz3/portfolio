0c4624a4d914d32506afc88431b1ebbaf98baf89
Johnson <johnson7267@gmail.com>
2022-02-23 22:46:32 -0500

    Initialized project

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,12 +32,14 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    // https://go.nuxtjs.dev/buefy
+    'nuxt-buefy',
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
