d09861d5e3ca7bfae9308495cd55274e33c9b9d6
Johnson <johnson7267@gmail.com>
2022-03-01 10:01:53 -0500

    fixed some css

--- layouts/about.vue
+++ layouts/about.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
     <section style="width: 100%; height: 100vh;">
-        <div v-if="window.width >= 600" class="item">
+        <div v-if="window.width >= 800" class="item">
             <AboutHeader/>
         </div>
         <div v-else class="item">
@@ -9,7 +9,7 @@
         </div>
         
         <div class="about-container">
-            I have recently found a passion for full stack development. This passion came from my previous relevant experiences. The feeling of having a web application, that I built, deployed, is one of the best feelings ever!
+            <!-- I have recently found a passion for full stack development. This passion came from my previous relevant experiences. The feeling of having a web application, that I built, deployed, is one of the best feelings ever! -->
         </div>
     </section>
     <section>
